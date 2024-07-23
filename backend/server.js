558c0b3066a188bc0b94b9711dca2cb6ba5844cd
LVSSandeepKumar <lvssandeepkumar12@gmail.com>
2024-07-23 20:24:44 +0530

    Backend Setup

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+
+import { ENV_VARS } from "./config/envVars.js";
+import authRoutes from "./routes/auth.route.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = ENV_VARS.PORT;
+
+app.use("/api/v1/auth", authRoutes);
+
+app.listen(PORT, (req,res) => {
+    console.log(`Server running at ${PORT}`)
+})
