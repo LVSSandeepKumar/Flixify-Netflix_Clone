8aeeb14c6e76d6a4989b9c03b442eafdd7e873a3
LVSSandeepKumar <lvssandeepkumar12@gmail.com>
2024-07-25 11:16:17 +0530

    Protect Route

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import { connectDB } from "./config/db.js";
 import authRoutes from "./routes/auth.route.js";
 import movieRoutes from "./routes/movie.route.js";
 import tvRoutes from "./routes/tv.route.js";
+import protectRoute from "./middleware/protectRoute.js";
 
 dotenv.config();
 
@@ -17,8 +18,8 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/v1/auth", authRoutes);
-app.use("/api/v1/movie", movieRoutes);
-app.use("/api/v1/tv", tvRoutes);
+app.use("/api/v1/movie",protectRoute, movieRoutes);
+app.use("/api/v1/tv",protectRoute, tvRoutes);
 
 app.listen(PORT, (req,res) => {
     console.log(`Server running at ${PORT}`);
