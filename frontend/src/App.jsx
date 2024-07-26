7b93ebe191db5ab38019709219389a066f7ed9be
LVSSandeepKumar <lvssandeepkumar12@gmail.com>
2024-07-26 16:26:34 +0530

    Built the home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { Navigate, Route, Routes } from "react-router-dom";
-import {Toaster} from "react-hot-toast";
+import { Toaster } from "react-hot-toast";
 import Footer from "./components/Footer";
 import HomePage from "./pages/home/HomePage";
 import LoginPage from "./pages/LoginPage";
@@ -9,31 +9,37 @@ import { useEffect } from "react";
 import { Loader } from "lucide-react";
 
 function App() {
-  const {user, isCheckingAuth, authCheck} = useAuthStore();
+  const { user, isCheckingAuth, authCheck } = useAuthStore();
   console.log(user);
   useEffect(() => {
     authCheck();
   }, []);
 
-  if(isCheckingAuth) {
+  if (isCheckingAuth) {
     return (
       <div className="h-screen">
         <div className="flex justify-center items-center bg-black h-full">
-          <Loader className="animate-spin text-red-500 size-10"/>
+          <Loader className="animate-spin text-red-500 size-10" />
         </div>
       </div>
-    )
+    );
   }
 
   return (
     <>
       <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/login" element={!user ? <LoginPage /> : <Navigate to={"/"} />} />
-      <Route path="/signup" element={!user? <SignupPage /> : <Navigate to={"/"} />} />
-    </Routes>
-    <Toaster />
-    <Footer/>
+        <Route path="/" element={<HomePage />} />
+        <Route
+          path="/login"
+          element={!user ? <LoginPage /> : <Navigate to={"/"} />}
+        />
+        <Route
+          path="/signup"
+          element={!user ? <SignupPage /> : <Navigate to={"/"} />}
+        />
+      </Routes>
+      <Toaster />
+      <Footer />
     </>
   );
 }
---
+++ frontend/src/components/HomeScreenNavbar.jsx
@@ -0,0 +1,87 @@
+import { LogOut, Menu, Search } from "lucide-react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { useAuthStore } from "../store/authUser";
+import { useContentStore } from "../store/content";
+
+const HomeScreenNavbar = () => {
+  const { user, logout } = useAuthStore();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const {setContentType} = useContentStore();
+
+  return (
+    <div className="max-w-6xl mx-auto flex flex-wrap items-center justify-between p-4 h-20">
+      {/* Desktop Navbar */}
+      <div className="flex items-center gap-10 z-50">
+        <Link to={"/"}>
+          <img
+            src="/flixify-logo.png"
+            alt="flixify logo"
+            className="w-32 sm:w-40"
+          />
+        </Link>
+        <div className="hidden sm:flex gap-3 items-center">
+          <Link to={"/"} className="hover:underline" onClick={() => setContentType("movie")}>
+            Movies
+          </Link>
+          <Link to={"/"} className="hover:underline" onClick={() => setContentType("tv")}>
+            TV Shows
+          </Link>
+          <Link to={"/history"} className="hover:underline">
+            Search History
+          </Link>
+        </div>
+      </div>
+
+      <div className="flex gap-2 items-center z-50">
+        <Link to={"/history"}>
+          <Search className="size-6 cursor-pointer" />
+        </Link>
+        <img
+          src={user.image}
+          alt="Avatar image"
+          className="h-8 rounded cursor-pointer"
+        />
+        <LogOut className="size-6 cursor-pointer" onClick={logout} />
+
+        <div className="sm:hidden">
+          <Menu className="size-6 cursor-pointer" onClick={toggleMenu} />
+        </div>
+      </div>
+
+      {/*Mobile Navbar */}
+      {isMenuOpen && (
+        <div className="w-full sm:hidden mt-4 z-50 bg-black border rounded border-gray-500">
+          <Link
+            to={"/"}
+            className="block hover:underline p-2"
+            onClick={toggleMenu}
+          >
+            Movies
+          </Link>
+          <Link
+            to={"/"}
+            className="block hover:underline p-2"
+            onClick={toggleMenu}
+          >
+            TV Shows
+          </Link>
+          <Link
+            to={"/history"}
+            className="block hover:underline p-2"
+            onClick={toggleMenu}
+          >
+            Search History
+          </Link>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default HomeScreenNavbar;
